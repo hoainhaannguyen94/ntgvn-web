031d6f9fbc3e62e87f1859f6ced667a08f065221
Hoai Nhan Nguyen <hoainhaannguyen@gmail.com>
2023-06-10 19:46:26 +0700

    Clean up

--- ntgvn-web/src/app/app.interceptor.ts
+++ ntgvn-web/src/app/app.interceptor.ts
@@ -1,10 +1,11 @@
 import { HttpRequest, HttpHandlerFn } from '@angular/common/http';
+import { environment } from '@environment';
 
 export function AppInterceptor(req: HttpRequest<unknown>,
     next: HttpHandlerFn) {
     const clonedRequest = req.clone({
         headers: req.headers
-            .set('Domain', 'now')
+            .set('Domain', environment.domain)
             .set('Authorization', localStorage.getItem('token') ?? '')
     })
     return next(clonedRequest);
